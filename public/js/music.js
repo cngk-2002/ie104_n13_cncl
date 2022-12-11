62e822b2c6c22d80edbd1d5c5677a983598973e2
tuanmiles <maianhtuan3765@gmail.com>
2022-12-11 15:29:18 +0700

    finish workflow

---
+++ public/js/music.js
@@ -0,0 +1,14 @@
+let loadMoreBtn = document.querySelector("#more-music-btn");
+let currentItem = 6;
+
+loadMoreBtn.onclick = () => {
+  let boxes = [...document.querySelectorAll(".container-fluid_1 .row .col")];
+  for (var i = currentItem; i < currentItem + 6; i++) {
+    boxes[i].style.display = "inline-block";
+  }
+  currentItem += 6;
+
+  if (currentItem >= boxes.length) {
+    loadMoreBtn.style.display = "none";
+  }
+};
