b6cb9ed0aa4584ebd28dc8eabca0c6f413c14a54
yamisora-sys <maianhtuan3765@gmail.com>
2022-12-11 23:10:22 +0700

    final

--- public/js/home.js
+++ public/js/home.js
@@ -1,30 +1,32 @@
-let loadMoreBtn = document.querySelector("#more-btn");
-let currentItem = 6;
+$(document).ready(function () {
+  let loadMoreBtn = document.getElementById('more-btn')
+  let currentItem = 6;
 
-loadMoreBtn.onclick = () => {
-  let boxes = [...document.querySelectorAll(".container-fluid_1 .row .col")];
-  for (var i = currentItem; i < currentItem + 6; i++) {
-    boxes[i].style.display = "inline-block";
-  }
-  currentItem += 6;
+  loadMoreBtn.onclick = () => {
+    let boxes = [...document.querySelectorAll(".container-fluid_1 .row .col")];
+    for (var i = currentItem; i < currentItem + 6; i++) {
+      boxes[i].style.display = "inline-block";
+    }
+    currentItem += 6;
 
-  if (currentItem >= boxes.length) {
-    loadMoreBtn.style.display = "none";
-  }
-};
+    if (currentItem >= boxes.length) {
+      loadMoreBtn.style.display = "none";
+    }
+  };
 
 //search sscript
 
-var search = document.getElementById('search-bar')
+  var search = document.getElementById('search-bar')
 
-search.addEventListener('change',(e) =>{
-  let searchValue = e.target.value;
-  let data = document.querySelectorAll("[data-search='famous-people']")
-  data.forEach((item)=>{
-    if (item.innerText.toLowerCase().includes(searchValue.toLowerCase())) {
-      item.style.display = "inline-block";
-    } else {
-      item.style.display = "none";
-    }
+  search.addEventListener('change',(e) =>{
+    let searchValue = e.target.value;
+    let data = document.querySelectorAll("[data-search='famous-people']")
+    data.forEach((item)=>{
+      if (item.innerText.toLowerCase().includes(searchValue.toLowerCase())) {
+        item.style.display = "inline-block";
+      } else {
+        item.style.display = "none";
+      }
+    })
   })
-})
+});
